58ca0f49855feaff83f10ffb47880b3aeec5ffb0
Miron <oskroba.miron@gmail.com>
2021-04-21 19:43:18 +0100

    epaper displaying info from server with frequency ~0.5Hz

--- entry.py
+++ entry.py
@@ -27,6 +27,7 @@ logging.basicConfig(level=logging.DEBUG)
 epd = epd3in7.EPD()
 updatingPins=True
 updatingMain=True
+pausingMain=False
 updatingData=True
 RESET_SCREEN_PIN = 27  # board 13, BCM 27
 NEXT_SCREEN_PIN  = 22  # board 15, BCM 22
@@ -42,20 +43,29 @@ rowsDescriptions = [
     ['voltage','current','battery temp'],
     ['height back','height front','speed gps','speed pitot']
 ]
+rowsUnits = [
+    [' [V]',' [A]',' [°C]'],
+    [' [m]',' [m]',' [m/s]',' [m/s]']
+]
 tableLinesXY = [
 
     [
     [(0  , 1/3*h),(w  , 1/3*h)],
     [(0  , 2/3*h),(w  , 2/3*h)],
-    [(w*2/5, 0  ),(w*2/5, h  )]    # vertical line
+    [(w*1/2, 0  ),(w*1/2, h  )]    # vertical line
     ],
 
     [
     [(0  , 1/4*h),(w  , 1/4*h)],
     [(0  , 2/4*h),(w  , 2/4*h)],
     [(0  , 3/4*h),(w  , 3/4*h)],
-    [(w*2/5, 0  ),(w*2/5, h  )]    # vertical line
+    [(w*1/2, 0  ),(w*1/2, h  )]    # vertical line
     ]
+]
+zupa = dict()
+warzywa = [
+    ['-1','-1','-1'],
+    ['-1','-1','-1','-1']
 ]
 tmp = [i for i in range(5)]
 
@@ -71,18 +81,18 @@ def drawData(canvas, drawMode):
     offset = -1
     rows = -1
     if currentScreen == SCREEN_1:    # 3
-        offset = 35
+        offset = 30
         rows = 3
     elif currentScreen == SCREEN_2:  # 4
-        offset = 14
+        offset = 16
         rows = 4
 
     if drawMode == DRAW_LEFT_COL:
         for i in range(rows):
-                canvas.text((20  , i/rows*h+offset), rowsDescriptions[currentScreen][i], font = font24, fill = 0)
+            canvas.text((20  , i/rows*h+offset), rowsDescriptions[currentScreen][i], font = font24, fill = 0)
     elif drawMode == DRAW_RIGHT_COL:
         for i in range(rows):
-                canvas.text((w/2+20, i/rows*h+offset), str(tmp[i]), font = font24, fill = 0)
+            canvas.text((w/2+60, i/rows*h+offset), warzywa[currentScreen][i] + rowsUnits[currentScreen][i], font = font24, fill = 0)
 
 # display functions
 
@@ -96,50 +106,109 @@ def displayData(image):
     epd.display_1Gray(epd.getbuffer(image))
 
 # thread functions
+def displayer_on_frame(frame: RxFrame):
+    zupa.update({frame.frame_type: frame.value})
 
+# program generated data
+# def updateData():
+#     time.sleep(10) # wait for epaper init
+#     while(updatingData):
+#         for i in range(5):
+#             tmp[i] += .1234
+#         time.sleep(.4)
+
+# data from server
 def updateData():
-    while(updatingData):
-        for i in range(5):
-            tmp[i] += .1234
-        time.sleep(.4)
+    global zupa
+    time.sleep(10) # wait for epaper init
+    receiver = MessageReceiver(displayer_on_frame)
+    while updatingData:
+        for key, value in zupa.items():
+            for i in range(2):
+                cols = -1
+                if i == 0:
+                    cols = 3
+                elif i == 1:
+                    cols = 4
+                
+                for j in range(cols):
+                    if key == rowsDescriptions[i][j]:
+                        warzywa[i][j] = converter(str(value))
+                        # logging.info("\t{} -> {}".format(key, value))
+
+        # data is properly read from the server:
+        # for i in range(3):
+        #     logging.info(f'{i}->\t{warzywa[0][i]}')
+        # for i in range(4):
+        #     logging.info(f'{i}->\t{warzywa[1][i]}')
+        # logging.info("-----")
+        time.sleep(.66)
 
 def updatePins():
+    time.sleep(10) # wait for epaper init
+    global updatingMain, pausingMain, mainUpdater, currentScreen
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(RESET_SCREEN_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     GPIO.setup(NEXT_SCREEN_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     while(updatingPins):
         if GPIO.input(RESET_SCREEN_PIN): # board 13, BCM 27
-            logging.info("RESET_SCREEN_PIN - resetting partial")
-            reset(1,3)
+            logging.info("RESET_SCREEN_PIN---------------------")
+
+            pausingMain=True
+            resetEpaper()
+            time.sleep(2)
+            pausingMain=False
+                
+            logging.info("RESET_SCREEN_PIN-------RESTARTED----")
 
         if GPIO.input(NEXT_SCREEN_PIN): # board 15, BCM 22
-            logging.info("NEXT_SCREEN_PIN")
-            global currentScreen
+            logging.info("NEXT_SCREEN_PIN---------------------")
+            pausingMain=True
+
             if currentScreen == SCREEN_1:
                 currentScreen = SCREEN_2
             elif currentScreen == SCREEN_2:
                 currentScreen = SCREEN_1
-            swapScreen()
-            time.sleep(5)
 
-        logging.info("handling")
-        time.sleep(.25)
+            pausingMain=False
+            time.sleep(.75)
+            logging.info("SCREEN SWAPPED")
+
+        logging.info("handling pins")
+        time.sleep(.75)
+    logging.info("updatePins - EXIT")
 
 # utility functions
+
+def updateScreen():
+    img = Image.new('L', (epd.height, epd.width), 0xFF)  # 0xFF: clear the frame
+    canvas = ImageDraw.Draw(img)
+    time.sleep(.05)
+    drawTable(canvas)
+    time.sleep(.05)
+    drawData(canvas, DRAW_RIGHT_COL)
+    time.sleep(.05)
+    drawData(canvas, DRAW_LEFT_COL)
+    time.sleep(.05)
+    displayData(img)
 
 def reset(mode, delay=0):
     epd.init(mode)
     epd.Clear(0xFF, mode)
     time.sleep(delay)
 
-def swapScreen():
-    image = Image.new('L', (epd.height, epd.width), 0xFF)  # 0xFF: clear the frame
-    canvas = ImageDraw.Draw(image)
+def resetEpaper():
+    global epd
+    epd = epd3in7.EPD()
 
-    drawTable(canvas)
-    drawData(canvas, DRAW_LEFT_COL)
-    display(image)
-
+    logging.info("init and Clear, NOW WAIT")
+    time.sleep(.2)
+    reset(0, 1)
+    reset(0, 2)
+    reset(1, 1)
+    reset(1, 2)
+    # update screen before loop 
+    updateScreen()
 
 def quitEpaper():
     time.sleep(1)
@@ -152,54 +221,55 @@ def quitEpaper():
     epd3in7.epdconfig.module_exit()
     exit()
 
+def converter(variable):
+    my_float_number=float(variable)
+    formated_float_number = '{:.2f}'.format(my_float_number)
+    x = repr(formated_float_number).replace('\'','')
+    return x
 
-def restartEpaper():
+def runEpaper():
     global currentScreen
     try:
-        logging.info("init and Clear, NOW WAIT")
+        # reset current screen data...
+        resetEpaper()
+        # wait while its transmitting data...
         time.sleep(2)
-        reset(0, .1)
-        reset(0, .5)
-        
-        currentScreen = SCREEN_2
-        # user code start
-        swapScreen()
-        
-        reset(1, .2)
         # partial display
         while(updatingMain):
-            dataImage = Image.new('L', (epd.height, epd.width), 0xFF)  # 0xFF: clear the frame
-            dataCanvas = ImageDraw.Draw(dataImage)
-            drawData(dataCanvas, DRAW_RIGHT_COL)
-            displayData(dataImage)
-            time.sleep(.25)
+            if pausingMain:    
+                logging.info("robotnicy stop---cos sie dzieje---")
+            else:
+                updateScreen()
+            time.sleep(.2)
         
-        logging.info("--------end--------")
+        logging.info("end")
         quitEpaper()
         
     except IOError as e:
         logging.info(e)
         quitEpaper()
         
-    except KeyboardInterrupt:    
+    except KeyboardInterrupt:
+        global updatingPins, updatingData
         logging.info("ctrl + c:")
         updatingPins=False
         updatingData=False
-        # quitEpaper()
+        quitEpaper()
 
 # MAIN
-# declare threads
-pinUpdater = threading.Thread(target=updatePins, args=())
-dataUpdater = threading.Thread(target=updateData, args=())
-mainUpdater = threading.Thread(target=restartEpaper, args=())
+if __name__ == '__main__':
+    # declare threads
+    pinUpdater = threading.Thread(target=updatePins, args=())
+    dataUpdater = threading.Thread(target=updateData, args=())
+    mainUpdater = threading.Thread(target=runEpaper, args=())
 
-# run threads
-pinUpdater.start()
-dataUpdater.start()
-mainUpdater.start()
+    # run threads
+    pinUpdater.start()
+    dataUpdater.start()
+    mainUpdater.start()
 
-# join threads
-pinUpdater.join()
-mainUpdater.join()
-dataUpdater.join()
+    # join threads
+    mainUpdater.join()
+    pinUpdater.join()
+    dataUpdater.join()
 
--- workspace.sh
+++ workspace.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "opening.."
-code entry.py config_link.py epd3in7_link.py mqtt-pushsampledata/main.py sbtPythonCan/sampleDisplayer.py
+code entry.py config_link.py epd3in7_link.py mqtt-pushsampledata/main.py sbtPythonCan/sampleDisplayer.py sbtPythonCan/read_from_mqtt.py
